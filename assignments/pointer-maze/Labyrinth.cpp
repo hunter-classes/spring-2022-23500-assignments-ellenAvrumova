158f81883f1a7d5a10c2772fb7017420874f549c
ellenAvrumova <e.avrumova@gmail.com>
2022-03-24 17:36:08 -0400

    Started milestone 1

--- assignments/pointer-maze/Labyrinth.cpp
+++ assignments/pointer-maze/Labyrinth.cpp
@@ -1,8 +1,48 @@
 #include "Labyrinth.h"
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
+    bool foundSpellbook = false;
+    bool foundPotion = false;
+    bool foundWound = false;
+
+    for(int i = 0; i < moves.length(); i++) {
+        if(moves.at(i) == 'N') {
+            if(start->north == nullptr) {
+                return false;
+            }
+        }
+        if(moves.at(i) == 'S') {
+            if(start->south == nullptr) {
+                return false;
+            }
+        }
+        if(moves.at(i) == 'E') {
+            if(start->east == nullptr) {
+                return false;
+            }
+        }
+        if(moves.at(i) == 'W') {
+            if(start->west == nullptr) {
+                return false;
+            }
+        }
+        if(start->whatsHere == Item::SPELLBOOK) {
+            foundSpellbook = true;
+        }
+        if(start->whatsHere == Item::POTION) {
+            foundSpellbook = true;
+        }
+        if(start->whatsHere == Item::WAND) {
+            foundSpellbook = true;
+        }
+    }
+
+    if(foundSpellbook && foundPotion && foundWound) {
+            return true;
+    }
     return false;
 }
+    /* TODO: Delete this comment and the remaining lines, then implement this function. */
+    // (void) start;
+    // (void) moves;
+    // return false;
