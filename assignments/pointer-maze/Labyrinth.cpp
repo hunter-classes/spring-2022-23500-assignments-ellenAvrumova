09e7e6b1787e2fade1baef920a186c8a00e4ba6a
ellenAvrumova <e.avrumova@gmail.com>
2022-03-24 22:12:43 -0400

    Path for default maze

--- assignments/pointer-maze/Labyrinth.cpp
+++ assignments/pointer-maze/Labyrinth.cpp
@@ -3,42 +3,60 @@
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
     bool foundSpellbook = false;
     bool foundPotion = false;
-    bool foundWound = false;
+    bool foundWand = false;
 
     for(int i = 0; i < moves.length(); i++) {
+        if(start->whatsHere == Item::SPELLBOOK) {
+            foundSpellbook = true;
+        }
+        if(start->whatsHere == Item::POTION) {
+            foundPotion = true;
+        }
+        if(start->whatsHere == Item::WAND) {
+            foundWand = true;
+        }
+
         if(moves.at(i) == 'N') {
             if(start->north == nullptr) {
                 return false;
             }
+            start = start->north;
         }
         if(moves.at(i) == 'S') {
             if(start->south == nullptr) {
                 return false;
             }
+            start = start->south;
         }
         if(moves.at(i) == 'E') {
             if(start->east == nullptr) {
                 return false;
             }
+            start = start->east;
         }
         if(moves.at(i) == 'W') {
             if(start->west == nullptr) {
                 return false;
             }
+            start = start->west;
         }
+
         if(start->whatsHere == Item::SPELLBOOK) {
             foundSpellbook = true;
         }
         if(start->whatsHere == Item::POTION) {
-            foundSpellbook = true;
+            foundPotion = true;
         }
         if(start->whatsHere == Item::WAND) {
-            foundSpellbook = true;
+            foundWand = true;
+        }
+
+        if(foundSpellbook && foundPotion && foundWand) {
+            return true;
         }
     }
-
-    if(foundSpellbook && foundPotion && foundWound) {
-            return true;
+    if(foundSpellbook && foundPotion && foundWand) {
+        return true;
     }
     return false;
 }
