ed680402dfe357aef16aed5d022521516bc57ad2
ellenAvrumova <e.avrumova@gmail.com>
2022-03-25 18:41:11 -0400

    Found path for my name

--- assignments/pointer-maze/Main.cpp
+++ assignments/pointer-maze/Main.cpp
@@ -9,10 +9,10 @@
  * do NOT edit the value of kYourName. Changing kYourName will change which
  * maze you get back, which might invalidate all your hard work!
  */
-const std::string kYourName = "TODO: Replace this string with your name.";
+const std::string kYourName = "Ellen";
 
 /* Change these constants to contain the paths out of your mazes. */
-const std::string kPathOutOfNormalMaze = "SESSWENNENSESS";
+const std::string kPathOutOfNormalMaze = "SWNWSNESEESWENWWSWSEEE";
 const std::string kPathOutOfTwistyMaze = "TODO: Replace this string with your path out of the twisty maze.";
 
 int main() {
