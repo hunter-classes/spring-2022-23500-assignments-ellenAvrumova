91a95ab42512cc741f20391eebf3e4e79d8a8765
ellenAvrumova <e.avrumova@gmail.comm>
2022-04-23 21:51:33 -0400

    Video 2 task

---
+++ assignments/BSTree/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "Node.h"
+
+int main() {
+    /** Video 2 Code
+    Node *n = new Node(20);
+    std::cout << n->getData() << std::endl;
+    Node *n2 = new Node(30);
+    n->setLeft(n2);
+    n2 = new Node(40);
+    n->setRight(n2);
+
+    std::cout << n->getLeft()->getData() << std::endl;
+    std::cout << n->getRight()->getData() << std::endl;
+    */
+
+   //Video 2 Task
+   Node *n = new Node(10);
+   Node *n2 = new Node(20);
+   Node *n3 = new Node(30);
+   n->setLeft(n2);
+   n->getLeft()->setLeft(n3);
+   Node *n4 = new Node(40);
+   n->getLeft()->getLeft()->setLeft(n4);
+   Node *n5 = new Node(50);
+   n->setRight(n5);
+   Node *n6 = new Node(60);
+   n->getRight()->setRight(n6);
+   Node *n7 = new Node(70);
+   n->getRight()->setLeft(n7);
+
+   std::cout << n->getData() << std::endl; //10
+   std::cout << n->getLeft()->getData() << " " << n->getLeft()->getLeft()->getData() << " " << n->getLeft()->getLeft()->getLeft()->getData() << std::endl; //20 30 40
+   std::cout << n->getRight()->getData() << " " << n->getRight()->getRight()->getData() << " " << n->getRight()->getLeft()->getData() <<std::endl; //50 60 70
+
+   
+
+   return 0;
+}
